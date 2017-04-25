5bd7cb4679f1f40dcf3cef88fae3e9f66f130786
Kenneth Hamilton <ken@kenhamilton.tech>
2017-04-25 14:05:27 -0400

    Initial AngularJS conversion

--- quotejoliservice/App_Start/BundleConfig.cs
+++ quotejoliservice/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace quotejoliservice
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/frameworks").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/angular.min.js",
+                        "~/Scripts/angular-material/angular-material.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -22,7 +24,8 @@ namespace quotejoliservice
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                       "~/Scripts/knockout-{version}.js",
-                      "~/Scripts/app.js"));
+                      "~/Scripts/Controllers/AddQuoteController.js",
+                      "~/Scripts/app-quotejoli.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
